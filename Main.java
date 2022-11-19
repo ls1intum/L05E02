64478d50bdec9f74fbcfb42434102859f18331e1
Santiago Berrezueta <88991954+Santia-go@users.noreply.github.com>
2022-11-19 07:37:50 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-
-
 class Shape {
     protected String color;
     public double area() {
@@ -7,8 +5,6 @@ class Shape {
         //TODO: calculation of the area of any figure
         return area;
     }
-
-
 }
 
 //SubClass of Shape
@@ -20,32 +16,31 @@ class Circle extends Shape {
         this.radius = radius;
 
     }
-    //formula to calculate the area
+    //Formula to calculate the area
     public double area() {
         return Math.PI * radius * radius;
     }
 }
 
-//Create a new subclass Rectangle of the superclass Shape
-    //Implement a constructor based on the width and height of the rectangle
-    //Override the area() method with the correct calculation
+//TODO: Create a new subclass Rectangle of the superclass Shape
+    //  - Implement a constructor based on the width and height of the rectangle
+    //  - Override the area() method with the correct calculation
 
-//Create a new subclass Square
-    //Implement a meaningful constructor
-    //Question: should Square extend Shape or Rectangle?
+//TODO: Create a new subclass Square
+    //  - Implement a meaningful constructor
+    //  - Question: should Square extend Shape or Rectangle?
 
 public class Main {
 
     public static void main(String[] args) {
 
-//Test your code
-        //Rectangle rectangle = new Rectangle(5, 7);
-        //System.out.println("Rectangle area: " + rectangle.area());
-        //Square square = new Square(5);
-        //System.out.println("Square area: " + square.area());
-        //Circle circle = new Circle(3);
-        //System.out.println("Circle area: " + circle.area());
-
+        /*Test your code
+        Rectangle rectangle = new Rectangle(5, 7);
+        System.out.println("Rectangle area: " + rectangle.area());
+        Square square = new Square(5);
+        System.out.println("Square area: " + square.area());
+        Circle circle = new Circle(3);
+        System.out.println("Circle area: " + circle.area());
+        */
     }
 }
-
